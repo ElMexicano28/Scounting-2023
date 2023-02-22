fdd2ab2b03a7250efffb37abe3be728bf34db365
ryanhubbs <113469956+ryanhubbs@users.noreply.github.com>
2023-02-22 08:52:08 -0500

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>2152 SMASH Scouting</h1>
+  <img src="" alt="Image failed to load.">
   <h2>Scouting Forms</h2>
   <ul v-if="list.length > 0" class="link-list">
     <li v-for="[i, name] of list.entries()" :key="i">
