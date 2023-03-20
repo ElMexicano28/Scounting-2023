657ed5863ce39e4ff30b93fd4fe5e109e98c8fef
ryanhubbs <113469956+ryanhubbs@users.noreply.github.com>
2023-03-20 13:16:58 -0400

    Update Adrian.vue

--- src/components/Adrian.vue
+++ src/components/Adrian.vue
@@ -2,6 +2,11 @@
   <img :src="absoluteLogoPath" alt="Cannot load logo file" class="center" width="300" height="75">
 </template>
 
+<script setup lang="ts">
+// Fetch logo path
+const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/adrian.png`);
+</script>
+
 <style lang="postcss">
 .link-list {
   margin: 0;
