7abe5a318cf57d1c18c548bef9f14b5df9c525df
ryanhubbs <113469956+ryanhubbs@users.noreply.github.com>
2023-03-20 13:08:52 -0400

    Create AdrianView.vue

---
+++ src/views/AdrianView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="adrian-container">
+    <Suspense>
+      <template #default>
+        <HomePage />
+      </template>
+      <template #fallback>Loading...</template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HomePage from "@/components/Adrian.vue";
+</script>
+
+<style>
+#adrian-container {
+  display: grid;
+  justify-content: center;
+}
+</style>
