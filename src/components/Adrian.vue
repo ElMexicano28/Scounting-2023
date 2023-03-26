3a5b1a2d1619bfa02220f0a711cbdb1625580037
ryanhubbs <113469956+ryanhubbs@users.noreply.github.com>
2023-03-26 08:30:56 -0400

    Update Adrian.vue

--- src/components/Adrian.vue
+++ src/components/Adrian.vue
@@ -1,11 +1,19 @@
 <template>
   <div :id="funnies">
-    <img :src="absoluteLogoPath" alt="Cannot load logo file" class="center">
+    <img :src="adrian" alt="Cannot load logo file" class="center" width="1700" height="2000">
+    <br>
+    <img :src="caden" alt="Cannot load logo file" class="center" width="1700" height="2000">
+    <br>
   </div>
 </template>
 
 <script setup lang="ts">
 // Fetch logo path
+const adrian = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
+const caden = $computed(() => `${import.meta.env.BASE_URL}assets/caden.png`);
+const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
+const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
+const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
 const absoluteLogoPath = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
 </script>
 
