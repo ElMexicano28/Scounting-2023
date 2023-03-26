4a8268675fcaa87a221e2537b0e3c5367d5884d2
ryanhubbs <113469956+ryanhubbs@users.noreply.github.com>
2023-03-26 08:56:56 -0400

    Update Adrian.vue

--- src/components/Adrian.vue
+++ src/components/Adrian.vue
@@ -1,16 +1,18 @@
 <template>
   <div :id="funnies">
-    <img :src="adrian" alt="Cannot load logo file" class="center" width="1500" height="2000">
-    <br>
-    <img :src="caden" alt="Cannot load logo file" class="center" width="1500" height="2000">
-    <br>
+    <a href="https://elmexicano28.github.io/Scounting-2023/assets/Adrian.jpg">Adrian</a>
+    <a href="https://elmexicano28.github.io/Scounting-2023/assets/brian.png">Brian</a>
+    <a href="https://elmexicano28.github.io/Scounting-2023/assets/savanna.png">Savanna</a>
+    <a href="https://elmexicano28.github.io/Scounting-2023/assets/caden.png">Caden</a>
+    <a href="https://elmexicano28.github.io/Scounting-2023/assets/stears.png">Mr. Stears</a>
+    <a href="https://elmexicano28.github.io/Scounting-2023/assets/julien.png">Julien</a>
   </div>
 </template>
 
 <script setup lang="ts">
 // Fetch logo path
-const adrian = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
-const caden = $computed(() => `${import.meta.env.BASE_URL}assets/caden.png`);
+//const adrian = $computed(() => `${import.meta.env.BASE_URL}assets/Adrian.jpg`);
+//const caden = $computed(() => `${import.meta.env.BASE_URL}assets/caden.png`);
 </script>
 
 <style lang="postcss">
